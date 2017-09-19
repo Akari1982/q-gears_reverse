e5bbeb98da61fa172061df736a1cff135d784b40
Akari <bakari82@mail.ru>
2017-09-20 01:42:14 +0500

    Regexp replace with util.

--- utilites/asm_to_string/main.cpp
+++ utilites/asm_to_string/main.cpp
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <cstdlib>
+#include <regex>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+
 
 using namespace std;
 
@@ -7,69 +13,55 @@ using namespace std;
 
 int main()
 {
-    char* buffer;
-    int buffer_size;
-
-    FILE* file = fopen( "source.txt", "rb" );
-    fseek( file, 0, SEEK_END );
-    buffer_size = ftell( file );
-
-    buffer = ( char* )malloc( sizeof( char ) * buffer_size );
-
-    fseek( file, 0, SEEK_SET );
-    fread( buffer, sizeof( char ), buffer_size, file );
-    fclose( file );
-
-    bool start_parse = false;
-
-    char string_result[ 10000 ];
-    int string_result_pos = 0;
-
-    char string_new[ 100 ];
-    int string_new_pos = 0;
-
-    char string_old[ 100 ];
-    int string_old_pos = 0;
-
-    for( int pos = 0; pos < buffer_size; ++pos )
+    ifstream src( "source.txt");
+    ofstream out( "output.txt" );
+    if( !src || !out )
     {
-        char data = buffer[ pos ];
-
-        string_old[ string_old_pos ] = data;
-        ++string_old_pos;
-
-        if( start_parse == true && data != '\n' )
-        {
-            string_new[ string_new_pos ] = data;
-            ++string_new_pos;
-        }
-
-        if( data == '\t' )
-        {
-            start_parse = true;
-            continue;
-        }
-        if( data == '\n' || ( pos + 1 >= buffer_size ) )
-        {
-            for( int i = 0; i < string_new_pos; ++i )
-            {
-                string_result[ string_result_pos ] = string_new[ i ];
-                ++string_result_pos;
-            }
-            string_result[ string_result_pos ] = '\n';
-            ++string_result_pos;
-
-            string_new_pos = 0;
-            string_old_pos = 0;
-            start_parse = false;
-            continue;
-        }
+        cout << "Error opening files!" << endl;
+        return 1;
     }
 
-    remove( "output.txt" );
-    FILE* file_res = fopen( "output.txt", "ab" );
-    fwrite( string_result, sizeof( char ), string_result_pos, file_res );
-    fclose( file_res );
+    string str;
+    while( std::getline( src, str ) )
+    {
+        std::regex e ( "^[0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][\\t]l(hu|w|h|b|bu)\\s*([a-z][0-9a-z]), \\$([0-9a-z][0-9a-z][0-9a-z][0-9a-z])\\(([a-z][0-9a-z])\\)$" );
+        if( std::regex_match( str ,e ) )
+        {
+            std::string m1 = std::regex_replace( str, e, "$1" ); std::transform( m1.begin(), m1.end(), m1.begin(), ::tolower );
+            std::string m2 = std::regex_replace( str, e, "$2" ); std::transform( m2.begin(), m2.end(), m2.begin(), ::toupper );
+            std::string m3 = std::regex_replace( str, e, "$3" ); std::transform( m3.begin(), m3.end(), m3.begin(), ::tolower );
+            std::string m4 = std::regex_replace( str, e, "$4" ); std::transform( m4.begin(), m4.end(), m4.begin(), ::toupper );
+            std::stringstream ss;
+            ss << m2 << " = " << m1 << "[" << m4 << " + " << m3 << "];";
+            str = ss.str();
+        }
+
+        e = "^[0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][\\t]s(hu|w|h|b|bu)\\s*([a-z][0-9a-z]), \\$([0-9a-z][0-9a-z][0-9a-z][0-9a-z])\\(([a-z][0-9a-z])\\)$";
+        if( std::regex_match( str ,e ) )
+        {
+            std::string m1 = std::regex_replace( str, e, "$1" ); std::transform( m1.begin(), m1.end(), m1.begin(), ::tolower );
+            std::string m2 = std::regex_replace( str, e, "$2" ); std::transform( m2.begin(), m2.end(), m2.begin(), ::toupper );
+            std::string m3 = std::regex_replace( str, e, "$3" ); std::transform( m3.begin(), m3.end(), m3.begin(), ::tolower );
+            std::string m4 = std::regex_replace( str, e, "$4" ); std::transform( m4.begin(), m4.end(), m4.begin(), ::toupper );
+            std::stringstream ss;
+            ss << "[" << m4 << " + " << m3 << "] = " << m1 << "(" << m2 << ");";
+            str = ss.str();
+        }
+
+        e = "^[0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][0-9A-Z][\\t]s(hu|w|h|b|bu)\\s*zero, \\$([0-9a-z][0-9a-z][0-9a-z][0-9a-z])\\(([a-z][0-9a-z])\\)$";
+        if( std::regex_match( str ,e ) )
+        {
+            std::string m1 = std::regex_replace( str, e, "$1" ); std::transform( m1.begin(), m1.end(), m1.begin(), ::tolower );
+            std::string m2 = std::regex_replace( str, e, "$2" ); std::transform( m2.begin(), m2.end(), m2.begin(), ::tolower );
+            std::string m3 = std::regex_replace( str, e, "$3" ); std::transform( m3.begin(), m3.end(), m3.begin(), ::toupper );
+            std::stringstream ss;
+            ss << "[" << m3 << " + " << m2 << "] = " << m1 << "(0);";
+            str = ss.str();
+        }
+
+        str += "\n";
+        out << str;
+    }
 
     return 0;
 }
